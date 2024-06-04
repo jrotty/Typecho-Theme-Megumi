9f0aa543ed0cec99c721710344d810cfb43dcb7e
jrotty <bssf@qq.com>
2024-06-04 12:46:33 +0800

    更换主题路径写法
    使用`<?php $this->options->themeUrl(); ?>`即可调用主题资源路径

--- header.php
+++ header.php
@@ -3,9 +3,9 @@
 	<head>
 		<meta charset="utf-8">
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="<?php $this->options->siteUrl(); ?>usr/themes/Megumi/other/style.css">
-		<link rel="stylesheet" href="<?php $this->options->siteUrl(); ?>usr/themes/Megumi/other/bootstrap/bootstrap-icons.css">
-		 <script src="<?php $this->options->siteUrl(); ?>usr/themes/Megumi/other/megumi.js"></script>
+		<link rel="stylesheet" href="<?php $this->options->themeUrl(); ?>other/style.css">
+		<link rel="stylesheet" href="<?php $this->options->themeUrl(); ?>other/bootstrap/bootstrap-icons.css">
+		 <script src="<?php $this->options->themeUrl(); ?>other/megumi.js"></script>
 		<meta http-equiv="content-type" content="text/html; charset=<?php $this->options->charset(); ?>" />
 		<?php $this->header(); ?>
 		<link rel="icon" href="<?php $this -> options -> logoUrl(); ?>" sizes="192x192"/>
@@ -18,17 +18,17 @@
 	    <style type="text/css"><?php $this -> options -> custom_css(); ?></style>
 
         
-        <link rel="stylesheet"href="<?php $this->options->siteUrl(); ?>usr/themes/Megumi/other/prism.css">
-        <script src="<?php $this->options->siteUrl(); ?>usr/themes/Megumi/other/prism.js"></script>
+        <link rel="stylesheet"href="<?php $this->options->themeUrl(); ?>other/prism.css">
+        <script src="<?php $this->options->themeUrl(); ?>other/prism.js"></script>
 	   
 	
 	    
 	    		 <!--灯箱-->
 		 <script type="text/javascript">$(document).ready(function () {$( ".fancybox").fancybox();});</script>
-        <script type="text/javascript" src="<?php $this->options->siteUrl(); ?>usr/themes/Megumi/other/jquery.min.js"></script> 
+        <script type="text/javascript" src="<?php $this->options->themeUrl(); ?>other/jquery.min.js"></script> 
         <!--如果主题已经引用了jQuery库，可以忽略这条-->
-        <link rel="stylesheet" href="<?php $this->options->siteUrl(); ?>usr/themes/Megumi/other/fancybox/jquery.fancybox.min.css">
-        <script src="<?php $this->options->siteUrl(); ?>usr/themes/Megumi/other/fancybox/jquery.fancybox.min.js"></script>
+        <link rel="stylesheet" href="<?php $this->options->themeUrl(); ?>other/fancybox/jquery.fancybox.min.css">
+        <script src="<?php $this->options->themeUrl(); ?>other/fancybox/jquery.fancybox.min.js"></script>
 
         <style>
         body:before{opacity: 0.1;}
@@ -56,10 +56,6 @@
 					
 					<?php $this->widget('Widget_Contents_Page_List')
 					   ->parse('<a href="{permalink}">{title}</a>'); ?>
-					
-					<?php if($this -> user -> hasLogin()): ?>
-                        <a href="<?php $this -> options -> adminUrl(); ?>" target="_blank">后台</a>
-                    <?php endif; ?>
 				</div>
 			</nav>
 	</header>
